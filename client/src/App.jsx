23db688f05a527ecce7d6bcd82031f0ab35f041a
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-05-08 06:45:01 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import ContactForm from './pages/ContactForm'
-import Footer from './component/Footer'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import ContactForm from './pages/ContactForm';
+// import Footer from './component/Footer';
+import Contact from './pages/contact/Contact';
 function App() {
   return (
-    <div className="App">
-      <ContactForm />
-      <Footer/>
+    <div className='App'>
+      {/* <ContactForm />
+      <Footer/> */}
+      <Contact />
     </div>
-  )
+  );
 }
-export default App
+export default App;
