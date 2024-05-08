8c318d236b46b56fe94c9233c573ef45488b1318
Momoh Abdulsamad <samadmomoh@gmail.com>
2024-05-08 06:45:14 +0100

    Update validateEnv.js

--- server/src/utils/validateEnv.js
+++ server/src/utils/validateEnv.js
@@ -1,9 +1,7 @@
-import {cleanEnv} from "envalid" //sets up environmental
-import { str, port } from "envalid/dist/validators.js";
+import { cleanEnv } from 'envalid'; //sets up environmental
+import { str, port } from 'envalid/dist/validators.js';
 
 export default cleanEnv(process.env, {
-    MONGODB_URL: str(),
-    PORT: port(),
-    
-    
+  MONGODB_URL: str(),
+  PORT: port(),
 });
